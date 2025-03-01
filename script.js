994a565c6c9394d429f8dbbfd932b270ac5ab59c
WhoJunaid <junaidmir0404@gmail.com>
2025-03-01 15:22:25 +0530

    new code

--- script.js
+++ script.js
@@ -1,199 +1,76 @@
-let textArea = document.getElementById("textarea");
-let button1= document.querySelector("#button1");
-let button2 = document.querySelector("#button2");
+let input1 = document.getElementById("inputarea"); 
 let output = document.getElementById("outputarea");
-let copyButton = document.getElementById("copy");
+let encryptButton = document.getElementById("button1");
+let decryptButton = document.getElementById("button2");
 let clearText = document.getElementById("button3");
-let inputPassword = document.getElementById("inputPassword6");
-let submitButton = document.getElementById("submitbutton");
-let message = document.getElementById("message");
-
-function getInput() {
-  var input = textArea.value;
-  
-  
-};
-
-
-const encryptCode = [
-  " " , "A" , "B" ,"C" ,"D","E","F","G","H","I","J","K","L","M","N","O","P" , "Q" , "R", "S", "T", "U", "V", "W", "X", "Y", "Z"
- ];
-
-
-
- var finalEncryption = []
-
-  function encrypt () {
-    
-    var input = textArea.value;
-    const encryptText = [...input]
-    
-    // console.log(encryptText);
-    
-    
-    
-      
-    
-      for (let index = 0; index < encryptText.length; index++) {
-       var elementTextBefore = encryptText[index];
-       
-        
-        
-
-
-         for (let index = 0; index < encryptCode.length; index++) {
-          
-       var elementEncrypt = encryptCode[index];
-      //  console.log(elementTextBefore);
-       
-        const elementText = elementTextBefore.toUpperCase()
-      // console.log(elementText )
-       
-         
-       if (elementText === elementEncrypt) {
-       var encryptNumber = encryptCode.indexOf(elementText);
-      //  console.log(encryptNumber);
-       finalEncryption.push(encryptNumber);
-        
-    
-       var encrypted = finalEncryption.toString()
-         var encrypted2 = finalEncryption.join("-");
-        // textinner.innerHTML = (finalEncryption)
-        output.value = encrypted2
-    } 
-       
-        
-      
-       
+let copyButton = document.getElementById("copy");
+const encryptCode = {
      
+    A:")", B:"(",C:"*" , D:"&" , E:"^", F:"%",G:"$",  H:"#" , I:"@" , J:"!" , K:"~" , L:"`" ,M:"}" ,N:"{" ,O:"]" ,P:"[" ,Q:"'" ,R:":" ,S:";" ,T:"?" ,U:"/" ,V:">" ,W:"<" ,X:"." ,Y:"," ,Z:"|",
+    " ":"_" 
+}
+
+encryptButton.onclick= ()  => {
+   
+    let outputCode = [];
+    let input = input1.value.toUpperCase();
+
+    for (let index = 0; index < input.length; index++) {
+    const encryptElement = input[index];
+     for (const key in encryptCode){
+
+        if (key === encryptElement) {
+            outputCode.push(encryptCode[key])
+            
+        }
      }
-
-     
-        
-
-      
-      }
-
-
-   
-  //  console.log(output.value);
-  //  console.log(encrypted2);
-  
   }
-button1.onclick = function Clicked1() {
-    // console.log("clicked1");
-    finalEncryption = []
-   getInput();
-   encrypt();
    
-   
-   
-
-
-    ;
-    
+    outputCode.toString();
+   output.value = outputCode.join("-");
     
 }
 
-button2.onclick = function Clicked2() {
-    console.log("clicked2");
-    finalDecryption = []
-    decryption();
-
+decryptButton.onclick = ()=>{
     
     
-}
-
-copyButton.onclick = function copy() {
-  output.select();
-  output.setSelectionRange(0,99999);
-  navigator.clipboard.writeText(output.value)
-
-
-  // console.log("copy");
-  
-  
-}
-let encryptMore = "Ayesha"
-function decryption() {
-  var input = textArea.value;
-  // console.log(input);
-  const decryptText = input.split("-")
-  // console.log(decryptText);
-
-  for (let index = 0; index < decryptText.length; index++) {
-    const decryptElement = decryptText[index];
-    // console.log(decryptElement);
+    let outputCode = [];
+    let decryptCode = input1.value.split("-");
+   
     
-
-    for (let index = 0; index < encryptCode.length; index++) {
-      const encryptCodeCharacter2 = encryptCode[index];
-      let encryptCodeIndex = encryptCode.indexOf(encryptCodeCharacter2);
-      // console.log(encryptCodeIndex);
+    for (let index = 0; index < decryptCode.length; index++) {
+        const decryptElement = decryptCode[index];
       
-      
-      
-
-      if (decryptElement==encryptCodeIndex) {
-        // console.log(`found `+  decryptElement);
-        let finalText  = encryptCode.at(index = decryptElement);
-        finalDecryption.push(finalText)
         
-
+        for (const key2 in encryptCode) {
+            if (encryptCode[key2]=== decryptElement) {
+                outputCode.push(key2);
+               
+                
+            }
+            
+        }
         
-      }
-         
-
-
-      
- var decrypted = finalDecryption.toString()
-   var decrypted2 = finalDecryption.join("");
-  
-  output.value = decrypted2
-      
     }
+    outputCode.toString();
+   output.value = outputCode.join("");
   
-  
-  }
 
-   
-   
-  
-  
-};
+
+}
 
 clearText.onclick = function clearTextClicked() {
-  // console.log("clear text");
-  textArea.value = [];
-  output.value = [];
-  
-} 
-
-
-// function checkPassword() {
-  
-//  }
-// submitButton.onclick = function submitClicked() {
-
-//  if (inputPassword.value === encryptMore ) {
-   
-//     message.innerText = 
-
-// "I really loved her, more than words could ever express. I might tell people that I’m okay, that I’ve moved on, but deep down, I know that’s not entirely true. Some days are easier than others, but there are moments when the memories come rushing back, and I feel the weight of everything we shared. Life has a way of moving forward, whether we’re ready or not, so I try to smile, to find happiness in the little things.So, be happy with what u have. 💕❤️"
-
-
+    input1.value = [];
+    output.value = [];
     
-//   }else{
-   
-//     message.innerText = "umm....its not right but life is good bro..Be happy"
-//   }
-  
- 
- 
-  
-// } 
+  } 
 
-
-  
+  copyButton.onclick = function copy() {
+    output.select();
+    output.setSelectionRange(0,99999);
+    navigator.clipboard.writeText(output.value)
+    
+    
+  }
   
   
